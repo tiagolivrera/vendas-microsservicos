0ae245aced0b5d3f1189809e15439a5c9f2fb782
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2023-07-01 20:21:46 -0300

    First commit

---
+++ VendaService/src/test/java/br/com/tiagolivrera/vendas/online/VendaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagolivrera.vendas.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
